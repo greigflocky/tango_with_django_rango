554f7aaf99f53883f5b74e2122f6eaa42cc66677
greig_flockhart <greigflockhart@gmail.com>
2021-04-22 20:23:42 +0100

    Adding chapter 4 instructions before attempting exercises

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Rango says hey there partner!" "<p><a href='/rango/about/'>About</a></p>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
     return HttpResponse("Rango says here is the about page." "<p><a href='/rango/'>Index</a></p>")
