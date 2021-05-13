fb87d610354783b61fe3495be20f39ddc9553ea6
greig_flockhart <greigflockhart@gmail.com>
2021-05-13 01:06:53 +0100

    Adding chapter 6 with completed tests

--- rango/admin.py
+++ rango/admin.py
@@ -5,11 +5,13 @@ from rango.models import Page, Category
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
-
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
 
 
 admin.site.register(Page, PageAdmin)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
+
 
 
 
